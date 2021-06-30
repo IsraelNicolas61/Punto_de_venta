f43908b44905b4680372f496552b9f468802d1a2
deuslux12 <39112635+deuslux12@users.noreply.github.com>
2021-06-30 16:10:47 -0500

    inico de proyecto
    Aqui empezamos con los modulos de datos y de ejecuciones del punto de venta

---
+++ PV1.0/Datos/ConexionSQL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class ConexionSQL
+    {
+        private const string V = "and contraseña ='";
+        static string conexionstring = "server = localhost:3306;database= PuntodeVenta;" +
+            "integrated security=true";
+
+        SqlConnection con = new SqlConnection(conexionstring);
+
+        public int consultalogin(string Usuario, String Contrasena)
+        {
+            int count;
+            con.Open();
+            string Query = "Select count(*)from persona where usuario =  '" + Usuario + "' " +
+                " and contraseña= '" + Contrasena + "'";
+            SqlCommand cmd = new SqlCommand(Query, con);
+            count = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+            return count;
+        }
+    }
+}
---
+++ PV1.0/PV1.0/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace PV1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_contrasena = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bnt_entrar = new System.Windows.Forms.Button();
+            this.bnt_salida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(84, 125);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(253, 20);
+            this.txt_usuario.TabIndex = 0;
+            // 
+            // txt_contrasena
+            // 
+            this.txt_contrasena.Location = new System.Drawing.Point(84, 192);
+            this.txt_contrasena.Name = "txt_contrasena";
+            this.txt_contrasena.Size = new System.Drawing.Size(253, 20);
+            this.txt_contrasena.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "contraseña";
+            // 
+            // bnt_entrar
+            // 
+            this.bnt_entrar.Location = new System.Drawing.Point(84, 246);
+            this.bnt_entrar.Name = "bnt_entrar";
+            this.bnt_entrar.Size = new System.Drawing.Size(75, 23);
+            this.bnt_entrar.TabIndex = 5;
+            this.bnt_entrar.Text = "ENTRAR";
+            this.bnt_entrar.UseVisualStyleBackColor = true;
+            this.bnt_entrar.Click += new System.EventHandler(this.bnt_entrar_Click);
+            // 
+            // bnt_salida
+            // 
+            this.bnt_salida.Location = new System.Drawing.Point(262, 246);
+            this.bnt_salida.Name = "bnt_salida";
+            this.bnt_salida.Size = new System.Drawing.Size(75, 23);
+            this.bnt_salida.TabIndex = 6;
+            this.bnt_salida.Text = "SALIDA";
+            this.bnt_salida.UseVisualStyleBackColor = true;
+            this.bnt_salida.Click += new System.EventHandler(this.bnt_salida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 314);
+            this.Controls.Add(this.bnt_salida);
+            this.Controls.Add(this.bnt_entrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_contrasena);
+            this.Controls.Add(this.txt_usuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.bnt_entrar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_contrasena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bnt_entrar;
+        private System.Windows.Forms.Button bnt_salida;
+    }
+}
+
